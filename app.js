494cc193da55f19d496ea2f027d3bb571903c166
vishal-srj10 <96437593+vishal-srj10@users.noreply.github.com>
2022-02-12 22:27:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,18 +37,16 @@ class App extends Component {
     ]
   }
 
-  render() {
+   render() {
     return (
       <div id="main">
         <ol>
-          {this.cityList.filter( (city)=>city.country=='India').map( (city,index)=>{
-            const key = `location${index + 1}`
-            return <li key={key}>{city.name}</li>;
+          {this.tempArr.map((element, index) => {
+            return <li key={`location${index + 1}`}>{element.name}</li>;
           })}
         </ol>
-        
       </div>
-    )
+    );
   }
 }
 
